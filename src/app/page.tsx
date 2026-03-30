6e01436437062c43aec05889d1cb485345c829b8
timothy <timothymatudi@outlook.com>
2026-03-30 17:57:36 +0100

    Simple redirect to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import { redirect } from 'next/navigation'
-import { createClient } from '@/lib/supabase/server'
 
-export default async function Page() {
-  const supabase = await createClient()
-  const { data: { user } } = await supabase.auth.getUser()
-
-  if (user) {
-    redirect('/dashboard')
-  } else {
-    redirect('/auth/login')
-  }
+export default function Page() {
+  redirect('/auth/login')
 }
